c872fbf014c5f8318ca81ea2dd8e25c7106cc110
khushi <khushin0106@gmail.com>
2025-04-04 11:32:48 +0530

    tinymce editor update

--- src/component/RTE.jsx
+++ src/component/RTE.jsx
@@ -13,7 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey="4r73iee8nxhttri033dd1d1kddbl7qatlqmcwwkqbelfme9v"
+        apiKey={process.env.TINYMCE_API_KEY}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
